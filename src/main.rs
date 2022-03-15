869329c430d072d4687212df6b4cc2fe49c37180
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-03-15 15:53:02 +0100

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use arbitrary::Arbitrary;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let data = std::fs::read(PathBuf::from(file!()).parent().unwrap().join("Cargo.toml")).unwrap();
+    let mut unstructured = arbitrary::Unstructured::new(&data);
+
+    let ast = syn::Item::arbitrary(&mut unstructured);
+
+    println!("{ast}");
 }
